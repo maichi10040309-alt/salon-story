25a2a7c9bde5f41e2f6c963d40552df1459ed945
maichi10040309-alt <maichi10040309@gmail.com>
2026-09-05 17:54:04 +0900

    Add src/data/staff.js for Salon Story v0.1

---
+++ src/data/staff.js
@@ -0,0 +1,4 @@
+export const staff=[
+{id:'akari',name:'あかり',tech:45,service:75,sales:60,speed:55,stamina:60,popularity:55,salary:180000,trait:'接客上手',emoji:'👩🏻‍🦰'},
+{id:'mizuki',name:'美月',tech:80,service:45,sales:35,speed:50,stamina:55,popularity:50,salary:200000,trait:'技術派',emoji:'👩🏻'},
+{id:'sakura',name:'さくら',tech:55,service:55,sales:80,speed:65,stamina:50,popularity:60,salary:190000,trait:'営業上手',emoji:'👱🏻‍♀️'}];
